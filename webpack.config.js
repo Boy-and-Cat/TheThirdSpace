0d44eb79e2b8f3c44858bece4303aa5a766f72d7
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2025-02-17 00:16:22 -0800

    Added webpack and three.js.
    Make sure to run "npm run build" before committing and add "dist/app.bundle.js".

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './js/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+};
